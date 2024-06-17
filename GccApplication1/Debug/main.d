9c4b68612e115863498be5b57e2ea29175b68050
Jacek Świątko <jacekswiatko0@gmail.com>
2024-06-17 10:17:02 +0200

    Added missing functionalities

--- GccApplication1/Debug/main.d
+++ GccApplication1/Debug/main.d
@@ -1,70 +1,74 @@
 main.d main.o: .././main.cpp \
- b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
- b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
- b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
- b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
- b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
- B:\programy\MICROCHIP\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom328pb.h \
- b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
- b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
- b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
- b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
- b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
- b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
- b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h \
- b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
- b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
- b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
- b:\win_docs\pobrane\avrpcd8544-master\lcd\lcd.h \
- b:\win_docs\pobrane\avrpcd8544-master\lcd\lcd.cpp \
- b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
- b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
- b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
- b:\win_docs\pobrane\avrpcd8544-master\lcd\charset.cpp \
- b:\win_docs\pobrane\avrpcd8544-master\lcd\lcd.h
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ G:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom328pb.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\time.h \
+ ../libs/lcd/lcd.h ../libs/lcd/lcd.cpp \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h \
+ ../libs/lcd/charset.cpp ../libs/lcd/lcd.h
 
-b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\io.h:
 
-b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\sfr_defs.h:
 
-b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
 
-b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
 
-b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
 
-B:\programy\MICROCHIP\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom328pb.h:
+G:\Program\ Files\ (x86)\Atmel\Studio\7.0\Packs\atmel\ATmega_DFP\1.7.374\include/avr/iom328pb.h:
 
-b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\portpins.h:
 
-b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\common.h:
 
-b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\version.h:
 
-b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\fuse.h:
 
-b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\lock.h:
 
-b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\interrupt.h:
 
-b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h:
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\avr\pgmspace.h:
 
-b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
 
-b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
 
-b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
 
-b:\win_docs\pobrane\avrpcd8544-master\lcd\lcd.h:
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdlib.h:
 
-b:\win_docs\pobrane\avrpcd8544-master\lcd\lcd.cpp:
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\time.h:
 
-b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+../libs/lcd/lcd.h:
 
-b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+../libs/lcd/lcd.cpp:
 
-b:\programy\microchip\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
 
-b:\win_docs\pobrane\avrpcd8544-master\lcd\charset.cpp:
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
 
-b:\win_docs\pobrane\avrpcd8544-master\lcd\lcd.h:
+g:\program\ files\ (x86)\atmel\studio\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
+
+../libs/lcd/charset.cpp:
+
+../libs/lcd/lcd.h:
--- GccApplication1/main.cpp
+++ GccApplication1/main.cpp
@@ -2,16 +2,20 @@
 #include <avr/interrupt.h>
 #include <avr/pgmspace.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define F_CPU 8000000
 
 #include "lcd.h"
 #include "lcd.cpp"
 
-#define F_CPU 8000000
-
 #define true 1
 #define false 0
 #define dimX 84
-#define  dimY 48
+#define dimY 46
+
+using namespace std;
 
 LCD lcd;
 
@@ -21,6 +25,22 @@ uint8_t INNER_TIMER_GAME = 0;
 uint8_t INNER_TIMER_SCORE = 0;
 uint8_t CURRENT_MIN = 0;
 uint8_t CURRENT_SEC = 0;
+
+
+int	snakeHeadX = 0;
+int	snakeHeadY = 0;
+int	fruitX = 0;
+int	fruitY = 0;
+int	snakeTailLength = 0;
+uint8_t	savedSeconds = 0;
+uint8_t	savedMinutes = 0;
+int snakeTailX[300];
+int snakeTailY[300];
+int score = 0;
+uint16_t game_over = 0;
+
+void updateSnake();
+void printSnake();
 
 void wait(){
 	volatile uint16_t i,j;
@@ -41,16 +61,50 @@ void print_start(){
 	lcd.writeString("to start :D",1); //letters : 11
 	lcd.render();
 }
+
 void print_over(){
+	char scoreString[5];
+	snprintf(scoreString, sizeof(scoreString), "%u", score);
+	
+	char secondsString[5];
+	snprintf(secondsString, sizeof(secondsString), "%u", savedSeconds);
+	
+	char minutesString[5];
+	snprintf(minutesString, sizeof(minutesString), "%u", savedMinutes);
+	
 	lcd.clear();
 	lcd.render();
 	
 	lcd.setCursor( 20, 0);
 	lcd.writeString("Game over ",1);
 	lcd.setCursor( 10, 10);
-	lcd.writeString("score   XXXX ",1);
+	lcd.writeString("score   ",1);
+	lcd.writeString(scoreString, 1);
 	lcd.setCursor( 10, 18);
-	lcd.writeString("time   XX:XX ",1);
+	lcd.writeString("time   ",1);
+	
+	if (savedMinutes <= 9)
+	{
+		lcd.writeString("0",1);
+		lcd.writeString(minutesString, 1);
+	}
+	else
+	{
+		lcd.writeString(minutesString, 1);
+	}
+	
+	lcd.writeString(":", 1);
+	
+	if (savedSeconds <= 9)
+	{
+		lcd.writeString("0",1);
+		lcd.writeString(secondsString, 1);
+	}
+	else
+	{
+		lcd.writeString(secondsString, 1);
+	}
+	
 	lcd.setCursor( 18, 30);
 	lcd.writeString("Press any",1);
 	lcd.setCursor( 0,40);
@@ -64,16 +118,16 @@ char detect_button(){
 	char dir = 'a';
 	switch(PINB){
 		case 0b10001110:
-		dir = 'w';
+		dir = 'd';
 		break;
 		case 0b10001101:
-		dir = 's';
-		break;
-		case 0b10001011:
 		dir = 'a';
 		break;
+		case 0b10001011:
+		dir = 's';
+		break;
 		case 0b10000111:
-		dir = 'd';
+		dir = 'w';
 		break;
 		default:
 		dir = LAST_DIRECTION;
@@ -115,13 +169,13 @@ void toggle_frame(){			// PRINT SNAKE FRAME
 	//	|					|
 	//  |___________________|
 
-	for (i = 0; i<=dimY-8; i++){
-		lcd.setPixel(0, i+8, 1);			// set top     Y line 
-		lcd.setPixel(dimX-1, i+8, 1);		// set bottom  Y line 
+	for (i = 1; i<=dimY-8; i++){
+		lcd.setPixel(0, i+7, 1);		// set left line 
+		lcd.setPixel(dimX-1, i+7, 1);		// set right line 
 	} 
-	for (i = 0; i< dimX;i++){
-		lcd.setPixel(i, 8 , 1);			// set left  X line
-		lcd.setPixel(i, dimY-1, 1);		// set right X line
+	for (i = 0; i<=dimX;i++){
+		lcd.setPixel(i, 8 , 1);			// set top line
+		lcd.setPixel(i, dimY-1, 1);		// set bottom line
 	}
 	lcd.render();
 }
@@ -155,24 +209,24 @@ void clear_scoreborard(){
 	lcd.render();
 }
 
-void change_scoreboard(uint16_t score){
+void change_scoreboard(uint16_t scoore){
 	char str[5];
-	snprintf(str, sizeof(str), "%u", score);
+	snprintf(str, sizeof(str), "%u", scoore);
 	
 	clear_scoreborard();		
 	
 	lcd.setCursor(dimX - 25,0);
 	
-	 if(score <=9 ){
+	 if(scoore <=9 ){
 			lcd.writeString( "000", 1);
 			lcd.writeString( str, 1);
-	}if(score <=99 && score > 9){
+	}if(scoore <=99 && scoore > 9){
 			lcd.writeString( "00", 1);
 			lcd.writeString( str, 1);
-	}if(score <=999 && score > 99){
+	}if(scoore <=999 && scoore > 99){
 			lcd.writeString( "0", 1);
 			lcd.writeString( str, 1);
-	}if(score > 999){
+	}if(scoore > 999){
 			lcd.writeString( "", 1);
 			lcd.writeString( str, 1);
 	}
@@ -186,6 +240,19 @@ void clear_time(){
 
 	for (i = 10; i< 60; i++){
 		for (j = 0 ; j<8 ;j++){
+			lcd.setPixel(i,j,0);
+		}
+	}
+	
+	lcd.render();
+}
+
+void clearSnakeFrame()
+{
+	unsigned int i,j;
+	
+	for (i = 1; i< 83; i++){
+		for (j = 9 ; j<45 ;j++){
 			lcd.setPixel(i,j,0);
 		}
 	}
@@ -268,7 +335,8 @@ ISR(TIMER0_COMPA_vect){
 		if(INNER_TIMER_SCORE == 63){
 			INNER_TIMER_SCORE = 0;
 			increment_time();
-			change_scoreboard(INNER_TIMER_GAME);
+			change_scoreboard(score);
+			printSnake();
 		}
 	}
 }
@@ -283,6 +351,143 @@ void stop_clock(bool var){ // if do == 1, CLOCK_STOP = 1, else CLOCK STOP = 0
 	}
 }
 
+void updateSnake( char & direction )
+{
+
+			int prevX = snakeTailX[0];
+			int prevY = snakeTailY[0];
+			int prev2X, prev2Y;
+			snakeTailX[0] = snakeHeadX;
+			snakeTailY[0] = snakeHeadY;
+			
+			for (int i = 1; i < snakeTailLength; i++)
+			{
+				prev2X = snakeTailX[i];
+				prev2Y = snakeTailY[i];
+				snakeTailX[i] = prevX;
+				snakeTailY[i] = prevY;
+				prevX = prev2X;
+				prevY = prev2Y;
+			}
+			
+			switch (direction) {
+				case 'a':
+				snakeHeadX--;
+				break;
+				case 'd':
+				snakeHeadX++;
+				break;
+				case 'w':
+				snakeHeadY--;
+				break;
+				case 's':
+				snakeHeadY++;
+				break;
+				default:
+				break;
+			}
+			
+			if (snakeHeadX > 26 || snakeHeadX < 0 || snakeHeadY > 11 || snakeHeadY < 0) //check for collisions with wall
+			{
+				game_over = 1;
+				savedMinutes = CURRENT_MIN;
+				savedSeconds = CURRENT_SEC;
+			}
+			
+			for (int i = 0; i < snakeTailLength; i++) //check for collisions with tail
+			{
+				if (snakeTailX[i] == snakeHeadX && snakeTailY[i] == snakeHeadY)
+				{
+					game_over = 1;
+					savedMinutes = CURRENT_MIN;
+					savedSeconds = CURRENT_SEC;
+				}
+			}
+			
+			if (snakeHeadX == fruitX && snakeHeadY == fruitY) //check for collisions with fruit
+			{
+				score += 10;
+				
+				while(true)
+				{
+					fruitX = rand() % 27;
+					fruitY = rand() % 12;
+					bool newFruitPositionIsOk = true;
+					
+					for(int i = 0; i<snakeTailLength; i++)
+					{
+						if(fruitX == snakeTailX[i] && fruitY == snakeTailY[i])
+						{
+							newFruitPositionIsOk = false;
+							break;
+						}
+					}
+					
+					if(newFruitPositionIsOk)
+					{
+						break;
+					}
+				}
+				
+				snakeTailLength++;
+			}
+			
+		
+	
+}
+
+void printSnake()
+{
+	clearSnakeFrame();
+	
+	//drawing head
+	// ###
+	// ###
+	// ###
+	
+	for(int i = ((snakeHeadX*3)+1); i<((snakeHeadX*3)+4); i++)
+	{
+		for(int ii = ((snakeHeadY*3)+9); ii<((snakeHeadY*3)+12); ii++)
+		{
+			lcd.setPixel(i, ii , 1);
+		}
+	}
+	
+	//drawing fruit
+	//  #
+	// # #
+	//  #
+	
+	for(int i = ((fruitX*3)+1); i<((fruitX*3)+4); i++)
+	{
+		for(int ii = ((fruitY*3)+9); ii<((fruitY*3)+12); ii++)
+		{
+			if(((i%2)==1 && (ii%2)==1) || ((i%2)==0 && (ii%2)==0))
+			{
+				lcd.setPixel(i, ii , 1);
+			}
+		}
+	}
+	
+	//drawing tail
+	// ###
+	// ###
+	// ###
+	
+	for(int j = 0; j<snakeTailLength; j++)
+	{
+		for(int i = ((snakeTailX[j]*3)+1); i<((snakeTailX[j]*3)+4); i++)
+		{
+			for(int ii = ((snakeTailY[j]*3)+9); ii<((snakeTailY[j]*3)+12); ii++)
+			{
+				lcd.setPixel(i, ii , 1);
+			}
+		}
+	}
+	
+	lcd.render();
+}
+
 int main(void)
 {
 	DDRB = 0b110000;
@@ -291,11 +496,10 @@ int main(void)
 	//DDRB = 0x77;
 	//PORTB = 0b111111;
 	
-	uint16_t score = 1519;
 	uint16_t game_start = 0;
-	uint16_t game_over = 0;
 	toggle_scr();
 	char current_direction = 'n';
+	srand(2137);
 	
 	while(1) {
 		print_start();
@@ -303,58 +507,105 @@ int main(void)
 		current_direction = 'n';
 		LAST_DIRECTION = 'n';
 		current_direction = detect_button();
+		
 		if (current_direction != 'n'){
 			game_start = 1;
+			
+			snakeHeadX = rand() % 27;
+			snakeHeadY = rand() % 12;
+			
+			while(true)
+			{
+				fruitX = rand() % 27;
+				fruitY = rand() % 12;
+				
+				if(fruitX != snakeHeadX || fruitY != snakeHeadY)
+				break;
+			}
+			
+			snakeTailLength = 0;
+			score = 0;
 		}
 		
-		if( game_start != 0){
+		if( game_start == 1)
+		{
 			lcd.clear();
 			
 			enable_lcd();
 			main_clock();
 			
-			while(1) {	
+			while(true)
+			{	
 				// main game loop
 				CLOCK_STOP = 0;
-			
+
+				current_direction = detect_button();
+				updateSnake( current_direction );
 				wait();
-				wait();
-				wait();
-				wait();
-				wait();			// if snake hit itself, game_over = 1
-				game_over = 1;
+				
+				// if snake hit itself, game_over = 1
+				//game_over = 1;
+				
 				if (game_over == 1){
 
 					lcd.clear();
 					lcd.render();
 					
 					CLOCK_STOP = 1;
-					
-					print_over();	
-					
 					game_start = 0;
 					game_over = 0;
-					current_direction = 'n';
-					LAST_DIRECTION = 'n';
 					
-					while(1){
+					while(true)
+					{
 						// wait for button press, to start over	
 						// wait for press of a button to exit to the main screen, if so, game_start = 1
 						
+						print_over();
+						wait();
+						current_direction = 'n';
+						LAST_DIRECTION = 'n';
 						current_direction = detect_button();
-						if (current_direction != 'n'){
+						
+						if (current_direction != 'n')
+						{
 							game_start = 1;
+							
+							snakeHeadX = rand() % 27;
+							snakeHeadY = rand() % 12;
+							
+							while(true)
+							{
+								fruitX = rand() % 27;
+								fruitY = rand() % 12;
+								
+								if(fruitX != snakeHeadX || fruitY != snakeHeadY)
+									break;
+							}
+							
+							snakeTailLength = 0;
+							score = 0;
 						}
 						
+						if(game_start == 1)
+						{
+							lcd.clear();
+							lcd.render();
+							toggle_frame();
+							toggle_scoreboard();
+							clear_time();
+							set_time();
+							lcd.render();
+							wait();
+							break;
+						}
 						
-						if(game_start != 0) {wait(); break;};
 					}
 				}
-				break;
+				
 			}
 		}
 		
-	};
+	}
 }
 
 
